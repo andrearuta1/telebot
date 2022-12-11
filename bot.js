aa51d466f912609052d8149ca2111f64072633bf
andrearuta1 <105165256+andrearuta1@users.noreply.github.com>
2022-12-11 21:59:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -154,7 +154,7 @@ db.mongoose.connect(db.url, {
 
 bot.launch({
 	webhook: {
-	  domain: 'https://telebot12345stellone.herokuapp.com',
+	  domain: 'https://telebot12345stellone.onrender.com',
 	  port: process.env.PORT || 3000
 	}
 });
