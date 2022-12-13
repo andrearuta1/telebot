8e0842f353f94caf3f9e9f10931e1d7d9ad40f3e
andrearuta1 <105165256+andrearuta1@users.noreply.github.com>
2022-12-14 00:09:18 +0100

    Update admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -23,14 +23,14 @@ module.exports = (bot) => {
         });
     });
 
-    bot.action('WARN_REPORT', commonActions.warn_report).catch((err, ctx) => {
-        console.error(err);
-        ctx.reply( '‼️ *ERRORE* ‼️\n\nCi scusiamo, si è verificato un errore. Riprova.',
-            { parse_mode: 'Markdown', reply_markup: { inline_keyboard : [[ Buttons.pannello_HOME ]]}}).catch((err) => {
-            console.log("ERRORE REPLY ERRORE ADMIN ROUTES");
-            console.error(err);
-        });
-    });
+    // bot.action('WARN_REPORT', commonActions.warn_report).catch((err, ctx) => {
+    //     console.error(err);
+    //     ctx.reply( '‼️ *ERRORE* ‼️\n\nCi scusiamo, si è verificato un errore. Riprova.',
+    //         { parse_mode: 'Markdown', reply_markup: { inline_keyboard : [[ Buttons.pannello_HOME ]]}}).catch((err) => {
+    //         console.log("ERRORE REPLY ERRORE ADMIN ROUTES");
+    //         console.error(err);
+    //     });
+    // });
 
     bot.action('BANNED_REPORT', commonActions.banned_report).catch((err, ctx) => {
         console.error(err);
