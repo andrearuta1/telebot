af90015d684a9469bfbbead09ee3a81d07cd9e84
andrearuta1 <105165256+andrearuta1@users.noreply.github.com>
2024-06-30 17:08:59 +0200

    change webhook domain

--- bot.js
+++ bot.js
@@ -154,7 +154,7 @@ db.mongoose.connect(db.url, {
 
 bot.launch({
 	webhook: {
-	  domain: 'https://telebot12345stellone.onrender.com',
+	  domain: 'https://telebot-70l8.onrender.com',
 	  port: process.env.PORT || 3000
 	}
 });
